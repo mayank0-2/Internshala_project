d31b6c6a4017dcdc238bce9572998d97dcd64c46
mayank0-2 <mayank02091999@gmail.com>
2023-06-15 12:38:04 +0530

    Get implemented

--- project/apimodule/models.py
+++ project/apimodule/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class apiModel(models.Model):
+    
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    address = models.CharField(max_length=100)
+    phone = models.IntegerField(null=True)
+    
+    
--- project/apimodule/views.py
+++ project/apimodule/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from  apimodule.serializer import model_serializer 
+from apimodule.models import apiModel
 # Create your views here.
+
+class apimoduleview(APIView) :
+    
+    def get(self, request, format=None) :
+        data = apiModel.objects.all()
+        serial = model_serializer(data, many = True)
+        return Response(serial.data)
+
+    
